340e39437eac6443dfe9ab9c41e0c59df765227f
roofi-ux <roofiii96@gmail.com>
2024-07-12 15:41:53 +0700

    add laravel project

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function index()
+    {
+        return view('login');
+    }
+    public function check(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            if (auth()->user()->status == 'admin') {
+                return redirect()->route('admin.dashboard.index');
+            } else {
+                return redirect()->route('user.dashboard.index')->with('success', 'Login Berhasil');
+            }
+        }
+        return back()->with('loginError', 'Username atau password salah!');
+    }
+    public function logout()
+    {
+        Auth::logout();
+        return redirect('/');
+    }
+}
